6638a142815f59edf987f959ee58e67a365ab300
Brenda Melo <brendarebeka.melo@gmail.com>
2023-05-15 16:10:48 -0300

    Walkthroug2 com responsividade

--- e-ticket/src/app/app-routing.module.ts
+++ e-ticket/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Walkthroug1Component } from './Components/walkthroug1/walkthroug1.component';
+import { Walkthroug2Component } from './Components/walkthroug2/walkthroug2.component';
 
-const routes: Routes = [];
+const routes: Routes = [ {
+  path: "",
+  pathMatch: "full",
+  redirectTo: "home"
+},
+{ path: 'home', component: Walkthroug1Component },
+{ path: 'passo1', component: Walkthroug1Component },
+{ path: 'passo2', component: Walkthroug2Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
